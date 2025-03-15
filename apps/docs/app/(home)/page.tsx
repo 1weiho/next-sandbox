fd459044fda1b418e60543ffd1c0fde618a95945
Yiwei Ho <davidho0403@gmail.com>
2025-03-15 22:09:42 +0800

    feat: home page

--- apps/docs/app/(home)/page.tsx
+++ apps/docs/app/(home)/page.tsx
@@ -2,18 +2,25 @@ import Link from 'next/link';
 
 export default function HomePage() {
   return (
-    <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{' '}
+    <main className="flex flex-1 flex-col justify-center items-center">
+      <h1 className="mb-4 text-4xl font-bold">Next Sandbox</h1>
+      <p className="mb-4 text-fd-muted-foreground">
+        A lightweight tool for testing and monitoring server actions in Next.js.
+      </p>
+      <div className="flex gap-4">
         <Link
           href="/docs"
-          className="text-fd-foreground font-semibold underline"
+          className="bg-fd-card-foreground px-4 py-2 rounded-2xl text-fd-primary-foreground"
         >
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
+          Get Started
+        </Link>
+        <Link
+          href="/docs/install"
+          className="bg-fd-secondary px-4 py-2 rounded-2xl text-fd-secondary-foreground"
+        >
+          Installation
+        </Link>
+      </div>
     </main>
   );
 }
--- apps/docs/app/layout.config.tsx
+++ apps/docs/app/layout.config.tsx
@@ -8,18 +8,19 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
  * Docs Layout: app/docs/layout.tsx
  */
 export const baseOptions: BaseLayoutProps = {
+  githubUrl: 'https://github.com/1weiho/next-sandbox',
   nav: {
     title: (
       <>
-        <svg
+        {/* <svg
           width="24"
           height="24"
           xmlns="http://www.w3.org/2000/svg"
           aria-label="Logo"
         >
           <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
+        </svg> */}
+        Next Sandbox
       </>
     ),
   },
