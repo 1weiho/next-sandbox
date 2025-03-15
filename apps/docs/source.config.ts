75eb2178b7f3cb009b2f4005d429296ebe933ba2
Yiwei Ho <davidho0403@gmail.com>
2025-03-15 22:35:55 +0800

    feat: package install code block

--- apps/docs/source.config.ts
+++ apps/docs/source.config.ts
@@ -1,3 +1,4 @@
+import { remarkInstall } from 'fumadocs-docgen';
 import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
 
 export const docs = defineDocs({
@@ -7,5 +8,6 @@ export const docs = defineDocs({
 export default defineConfig({
   mdxOptions: {
     // MDX options
+    remarkPlugins: [remarkInstall],
   },
 });
