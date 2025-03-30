2289cc392f53ce2ed9dbfb0030a03318166df0ee
Yiwei Ho <davidho0403@gmail.com>
2025-03-30 17:21:30 +0800

    chore: update fumadocs og primary text color

--- apps/docs/app/docs-og/[...slug]/route.tsx
+++ apps/docs/app/docs-og/[...slug]/route.tsx
@@ -3,6 +3,7 @@ import { metadataImage } from '../../../lib/metadata';
 
 export const GET = metadataImage.createAPI((page) => {
   return generateOGImage({
+    primaryTextColor: 'rgb(127, 187, 224)',
     title: page.data.title,
     description: page.data.description,
     site: 'Next Sandbox',
