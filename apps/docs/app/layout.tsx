b2d865ac4783463e85ff8cb8dcfe341cade2509f
Yiwei Ho <davidho0403@gmail.com>
2025-03-30 17:06:11 +0800

    chore: main layout metadata

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,11 +1,36 @@
 import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
+import type { Metadata } from 'next';
 import { Geist } from 'next/font/google';
 import type { ReactNode } from 'react';
 
 const geist = Geist({
   subsets: ['latin'],
 });
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Next Sandbox',
+    default: 'Next Sandbox',
+  },
+  description:
+    'A lightweight tool for testing and monitoring server actions in Next.js.',
+  openGraph: {
+    title: 'Next Sandbox',
+    description:
+      'A lightweight tool for testing and monitoring server actions in Next.js.',
+    url: 'https://next-sandbox.1wei.dev',
+    siteName: 'Next Sandbox',
+    images: '/banner.png',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@1weiho',
+    creator: '@1weiho',
+    images: '/banner.png',
+  },
+  metadataBase: new URL('https://next-sandbox.1wei.dev'),
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
